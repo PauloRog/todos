c2ea4b131a0535a50eb5f92ec748f2384d091146
Paulo Rogério <paulo-rpf@hotmail.com>
2021-09-29 23:57:01 -0300

    User creation route

--- src/index.js
+++ src/index.js
@@ -13,6 +13,47 @@ function verifyExistsUserAccount(request, response, next) {
 app.use(cors());
 app.use(expres.json());
 
-app.post('/users', (request, response) => {
-
+app.get('/', (request, response) => {
+  return response.json({
+    api: 'Todos',
+    createdBy: 'Paulo Rogério',
+    gretting: 'This api was created as a challenge for the boot camp ignite, in order to control everyone (Activities)',
+    baseUrl: 'http://localhost:3333',
+    routes: [
+      {
+        type: 'POST',
+        route: 'baseUrl/users',
+      },
+    ]
+  })
 });
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+  const usernameAlreadyExists = users.some(
+    (_user) => _user.username === username
+  )
+
+  if (usernameAlreadyExists) {
+    return response.status(400).json({ error: 'Username already exists!' });
+  }
+
+  if (!name || !username) {
+    return response.status(400).json({ error: 'name and username are required!' })
+  }
+  
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).send(user);
+});
+
+app.listen(3333, () => {
+  console.log('Server running! Go to: http://localhost:3333')
+})
